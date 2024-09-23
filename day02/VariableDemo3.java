9a32bfa9a87cdb1b4bf612f1014a1a2a33381665
a fish <gaoaodefish@gmail.com>
2024-09-23 17:59:23 +0800

    1

---
+++ day02/VariableDemo3.java
@@ -0,0 +1,38 @@
+public class VariableDemo3{
+	//主入口
+	public static void main(String[] args){
+		//byte
+		byte b = 10;
+		System.out.println(b);
+		//short
+		short s = 20;
+		System.out .println(s);
+		//int
+		int a = 30;
+		System.out.println(a);
+		//long 
+		//如果要定义long类型的bian变量
+		//在数据值后面需要加一个L作为后缀
+		//可以大写，可以小写，一般小写
+		long z = 99999999999L;
+		System.out.println(z);
+		
+		//float 
+		//注意点：定义flout类型的变量
+		//数据值也需要加一个F作为后缀
+		float f = 10.123f;
+		System.out.println(f);
+		//double 
+		double d = 20.234d;
+		System.out.println(d);
+		
+		//char 
+		char c = '中';
+		System.out.println(c);
+		
+		//boolean
+		//true false
+		boolean o = true;
+		System.out.println(o);
+	}
+}
